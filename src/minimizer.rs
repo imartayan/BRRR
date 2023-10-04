355ee7919f935719fc6db8ac47f632a9aff47c63
Igor Martayan <igor@martayan.org>
2023-10-04 17:23:28 +0200

    Let MinimizerQueue handle any hashable type

--- src/minimizer.rs
+++ src/minimizer.rs
@@ -1,46 +1,43 @@
-use crate::kmer::{Base, Kmer};
 use ahash::RandomState;
-use core::hash::{BuildHasher, Hasher};
+use core::hash::{BuildHasher, Hash, Hasher};
 use core::marker::PhantomData;
 use std::collections::VecDeque;
 
-pub struct MinimizerQueue<const M: usize, T: Base, MT: Kmer<M, T>> {
-    deq: VecDeque<(MT, u8)>,
+pub struct MinimizerQueue<const W: usize, T: Hash + Copy> {
+    deq: VecDeque<(T, u8)>,
     hash_builder: RandomState,
-    width: u8,
     time: u8,
     _phantom: PhantomData<T>,
 }
 
-impl<const M: usize, T: Base, MT: Kmer<M, T>> MinimizerQueue<M, T, MT> {
-    pub fn new_with_seed(k: usize, seed: usize) -> Self {
+impl<const W: usize, T: Hash + Copy> MinimizerQueue<W, T> {
+    pub fn new_with_seed(seed: usize) -> Self {
         Self {
-            deq: VecDeque::with_capacity(k - M + 1),
+            deq: VecDeque::with_capacity(W),
             hash_builder: RandomState::with_seed(seed),
-            width: (k - M + 1) as u8,
             time: 0,
             _phantom: PhantomData,
         }
     }
 
-    pub fn new(k: usize) -> Self {
-        Self::new_with_seed(k, k + M)
+    pub fn new() -> Self {
+        Self::new_with_seed(W)
     }
 
-    pub fn get_min(&self) -> MT {
+    pub fn get_min(&self) -> T {
         if self.deq.is_empty() {
             panic!("MinimizerQueue is empty")
         }
         self.deq[0].0
     }
 
-    fn hash(&self, u: MT) -> u64 {
+    fn hash(&self, u: T) -> u64 {
         let mut state = self.hash_builder.build_hasher();
         u.hash(&mut state);
         state.finish()
     }
 
-    pub fn insert(&mut self, u: MT) {
+    pub fn insert(&mut self, u: T) {
         let mut i = self.deq.len();
         while i > 0 {
             let (v, _) = self.deq[i - 1];
@@ -57,21 +54,23 @@ impl<const M: usize, T: Base, MT: Kmer<M, T>> MinimizerQueue<M, T, MT> {
             }
         }
         self.deq.push_back((u, self.time));
-        self.time = (self.time + 1) % self.width;
+        self.time = (self.time + 1) % W as u8;
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::kmer::RawKmer;
+    use crate::kmer::{Kmer, RawKmer};
+
+    const K: usize = 7;
+    const M: usize = 3;
+    const W: usize = K - M + 1;
+    type T = u8;
 
     #[test]
     fn test_minimizer() {
-        const K: usize = 7;
-        const M: usize = 3;
-        type T = u8;
-        let mut queue = MinimizerQueue::new(K);
+        let mut queue = MinimizerQueue::<W, _>::new();
         RawKmer::<M, T>::iter_from_nucs(b"AAATAGT".iter()).for_each(|mmer| {
             queue.insert(mmer);
         });
@@ -84,10 +83,7 @@ mod tests {
 
     #[test]
     fn test_hash() {
-        const K: usize = 7;
-        const M: usize = 3;
-        type T = u8;
-        let queue = MinimizerQueue::new(K);
+        let queue = MinimizerQueue::<W, _>::new();
         let u = RawKmer::<M, T>::from_nucs(b"ACT");
         let h1 = queue.hash(u);
         let h2 = queue.hash(u);
